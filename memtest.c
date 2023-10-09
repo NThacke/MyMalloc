b5987540131b7392c68db06e6740e842b46c1540
Nicholas C. Thacke <nct36@cd.cs.rutgers.edu>
2023-10-09 19:22:14 -0400

    added tests

--- memtest.c
+++ memtest.c
@@ -15,8 +15,61 @@ void test1() {
     }
     printf("Allocated %d times before OOM\n", i);
 }
+void test0() {
+    printf("~--------------~\n");
+    printf("|  Test Zero   |\n");
+    printf("~--------------~\n");
+
+    printf("********************************\n");
+    printf("Allocating eight byte of memory\n");
+    printf("********************************\n");
+
+    void * p = malloc(8);
+    print_mem();
+    printf("********************************\n");
+    printf("Allocating eight bytes of memory\n");
+    printf("********************************\n");
+    void *p2 = malloc(8);
+
+    print_mem();
+    printf("********************************\n");
+    printf("Allocating eight bytes of memory\n");
+    printf("********************************\n");
+
+    void * p3 = malloc(8);
+    print_mem();
+
+
+    printf("********************************\n");
+    printf("Freeing second allocated pointer\n");
+    printf("********************************\n");
+
+    free(p2);
+
+    print_mem();
+    
+    printf("********************************\n");
+    printf("Freeing first allocated pointer\n");
+    printf("********************************\n");
+
+    free(p);
+
+    print_mem();
+
+    printf("********************************\n");
+    printf("Freeing third allocated pointer\n");
+    printf("********************************\n");
+    free(p3);
+    print_mem();
+}
+
+void testa() {
+    void * p = malloc(8);
+    // void * p2 = malloc(8);
+    print_mem();
+    free(p);
+    print_mem();
+}
 int main() {
-    test1();
-
-
+    testa();
 }
